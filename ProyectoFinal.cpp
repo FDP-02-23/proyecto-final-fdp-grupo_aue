9086601e3f84c5b36d4f13f68be29ed623786620
ulisesnahonri <00191323@uca.edu.sv>
2023-11-18 18:19:34 -0600

    modificado

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -4,50 +4,7 @@
 using namespace std;
 
 //------------------------------FUNCIONES-----------------------------------------------------
-void MenuPrincipal(){
-
-}
-
-
-//-------------------------------MAIN------------------------------------------
-int main(){
-    limpiar();
-    MenuPrincipal();
-
-    return 0;
-}
-
-
-//inicio de secion administrador
-bool seccionAdmin() 
-{
-    string User, PassW;
-    int intentos = 0;
-    while (intentos < 3)
-    {
-        cout << "Ingrese el nombre de usuario: ";
-        cin >> User;
-        cout << "Ingrese la contrasenia: ";
-        cin >> PassW;
-
-        if (User == "admin" && PassW == "admin123")
-        {
-            return true;
-        }
-        else
-        {
-            intentos++;
-            cout << "Datos incorrectos. Intentos restantes: " << 3 - intentos << endl;
-        }
-    }
-
-    cout << "Ha excedido el numero de intentos. Reiniciando el programa..." << endl;
-    return false;
-}
-
-// codigo de funciones 
-
-  void MenuPrincipal(){ //funcion del menu principal
+void MenuPrincipal(){ //funcion del menu principal
 
     int opc=0;
 
@@ -141,4 +98,44 @@ bool seccionAdmin()
 
 
     }
+
+
+//-------------------------------MAIN------------------------------------------
+int main(){
+    limpiar();
+    MenuPrincipal();
+
+    return 0;
+}
+
+
+//inicio de secion administrador
+bool seccionAdmin() 
+{
+    string User, PassW;
+    int intentos = 0;
+    while (intentos < 3)
+    {
+        cout << "Ingrese el nombre de usuario: ";
+        cin >> User;
+        cout << "Ingrese la contrasenia: ";
+        cin >> PassW;
+
+        if (User == "admin" && PassW == "admin123")
+        {
+            return true;
+        }
+        else
+        {
+            intentos++;
+            cout << "Datos incorrectos. Intentos restantes: " << 3 - intentos << endl;
+        }
+    }
+
+    cout << "Ha excedido el numero de intentos. Reiniciando el programa..." << endl;
+    return false;
+}
+
+
+ 
  
